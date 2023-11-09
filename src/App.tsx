438f08fe4b21fbbdd8430c2a4bb6c72c74a0fbec
Mike Morrison <Mantis@LaurensMac.local>
2023-11-09 16:36:12 -0500

    Tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -4,31 +4,22 @@ import { Tweakable } from './Tweakable'
 function App() {
 
   return (
-    <div className='content'>
-      <header>
-        <nav>
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Blog</li>
-            <li>Contact</li>
-          </ul>
-        </nav>
-      </header>
+    <div className='content py-5'>
       <main>
         <section>
-          <div className="block">
-            <Tweakable tag="h2">This is an h1</Tweakable>
-            <Tweakable tag="p">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque totam voluptatibus, consectetur pariatur ut odio voluptates saepe ratione nihil esse tenetur aliquid autem eos cum ipsum dolore. Amet assumenda fuga autem, iusto tenetur quam, temporibus unde, velit maiores rem minima sequi. Debitis, veniam maxime voluptates necessitatibus itaque id autem quis?</Tweakable>
-          </div>
-          <div className="columns">
-            <div className="column">col</div>
-            <div className="column">col</div>
-            <div className="column">col</div>
-          </div>
+            <div className="container">
+              <Tweakable tag="h1">Lorem ipsum dolor sit amet.</Tweakable>
+              <Tweakable tag="p">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque totam voluptatibus, consectetur pariatur ut odio voluptates saepe ratione nihil esse tenetur aliquid autem eos cum ipsum dolore. Amet assumenda fuga autem, iusto tenetur quam, temporibus unde, velit maiores rem minima sequi. Debitis, veniam maxime voluptates necessitatibus itaque id autem quis?</Tweakable>
+              <Tweakable tag="ul">
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+              </Tweakable>
+            </div>
         </section>
       </main>
-      <footer></footer>
     </div>
   )
 }
--- src/Tweakable.tsx
+++ src/Tweakable.tsx
@@ -10,14 +10,23 @@ export const Tweakable = ({ tag = "h1", children }: Props) => {
     const [fontSize, setFontSize] = useState<string>("16");
     const [letterSpacing, setLetterSpacing] = useState<string>("0");
     const [lineHeight, setLineHeight] = useState<string>("1");
+    const [fontWeight, setFontWeight] = useState<string>("400");
 
     const Tag = tag
     return (
-        <Tag style={{fontSize: `${fontSize}px`, letterSpacing: `${letterSpacing}px`, lineHeight: lineHeight}}>
-            {children}
-            <input onChange={({target: { value }}) => setFontSize(value)} className="is-block" type="number" value={fontSize} min={12} max={4 * 24} step={4} />
-            <input onChange={({target: { value }}) => setLetterSpacing(value)} className="is-block" type="number" value={letterSpacing} min={-10} max={10} step={1} />
-            <input onChange={({target: { value }}) => setLineHeight(value)} className="is-block" type="number" value={lineHeight} min={1} max={1.65} step={.05} />
-        </Tag>
+        <div className="box">
+            <Tag style={{fontSize: `${fontSize}px`, letterSpacing: `${letterSpacing}px`, lineHeight: lineHeight, fontWeight: fontWeight}}>
+                {children}
+            </Tag>
+            <label htmlFor="font-size">Font Size</label>
+            <input id="font-size" onChange={({target: { value }}) => setFontSize(value)} type="number" value={fontSize} min={12} max={4 * 24} step={4} />
+            <label htmlFor="letter-spacing">Letter Spacing</label>
+            <input id="letter-spacing" onChange={({target: { value }}) => setLetterSpacing(value)} type="number" value={letterSpacing} min={-10} max={10} step={1} />
+            <label htmlFor="line-height">Line Height</label>
+            <input id="line-height" onChange={({target: { value }}) => setLineHeight(value)} type="number" value={lineHeight} min={1} max={1.65} step={.05} />
+            <label htmlFor="font-weight">Font Weight</label>
+            <input id="font-weight" onChange={({target: { value }}) => setFontWeight(value)} type="number" value={fontWeight} min={100} max={900} step={100} />
+        </div>
+        
     )
 }
